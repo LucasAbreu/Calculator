7f1d659b1cf8052f792659b2b1640bc48dbbca64
lucas <lucas@DESKTOP-DQPAIG6>
2018-08-22 22:58:26 -0300

    tentando mas nao deu

--- src/main/java/lucasabreu/Calculadora/Calculator.java
+++ src/main/java/lucasabreu/Calculadora/Calculator.java
@@ -2,9 +2,27 @@ package lucasabreu.Calculadora;
 
 public class Calculator {
 	public int evaluate(String expression) {
-		int sum = 0;
-		for (String summand : expression.split("\\+"))
-			sum += Integer.valueOf(summand);
-		return sum;
+		int sum = 0, nextAdd = 0, nextSub = 0, currChar = 0, nextChar = 0, nextNumber = 0;
+		while( currChar < expression.length() ) {
+			nextAdd = expression.indexOf("+", currChar);
+			nextSub = expression.indexOf("-", currChar);
+			if( ( nextAdd < nextSub ) && (nextAdd > -1) ) {
+				nextNumber = Integer.valueOf(expression.substring(currChar, nextAdd));
+				nextChar = nextAdd;
+			} else if( ( nextAdd > nextSub ) && (nextSub > -1) ) {
+				nextNumber = Integer.valueOf(expression.substring(currChar, nextSub));
+				nextChar = nextSub;
+			} else if( nextAdd == nextSub )
+				nextNumber = Integer.valueOf(expression.substring(currChar, expression.length()));
+			nextChar = expression.length();
+
+			if( expression.charAt(currChar) == '-' )
+				sum -= nextNumber;
+			else
+				sum += nextNumber;
+			currChar = nextChar;
+		}
+		return sum;	
 	}
+
 }
